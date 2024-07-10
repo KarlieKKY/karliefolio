58f22ea2753f59f07e206b94e9e505e139eff1fa
KarlieKKY <yanndeel@gmail.com>
2024-07-10 16:37:30 +0100

    placeholders

---
+++ app/projects/_components/Projects.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <section>
+      <h1>all project</h1>
+    </section>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Projects from "./_components/Projects";
+
+export default function Page() {
+  return (
+    <section>
+      <h1>project</h1>
+      <Projects />
+    </section>
+  );
+}
