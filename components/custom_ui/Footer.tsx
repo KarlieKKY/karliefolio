bfe0c873d2a24bef92f2b34035cf78ff4820be6a
KarlieKKY <yanndeel@gmail.com>
2024-07-24 14:17:38 +0100

    footer add style

--- components/custom_ui/Footer.tsx
+++ components/custom_ui/Footer.tsx
@@ -1,10 +1,33 @@
+import Image from "next/image";
+import { socialMediaIcons } from "@/utils/socialMedia-icons";
+import { Button } from "../ui/button";
+
 export default function Footer() {
   return (
-    <footer className="bg-neutral-900">
-      <div className="text-white">
-        <p>social media icons</p>
-        <p>copyright</p>
-        <p>© 2024 Karlie Guan. All rights reserved.</p>
+    <footer className="bg-neutral-900 text-white w-full">
+      <div className="bg-zinc-800 py-3 flex justify-center">
+        <Button className="font-semibold uppercase" variant="ghost">
+          download cv
+        </Button>
+      </div>
+      <div className="mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-10">
+        <div className="flex flex-col justify-between items-center h-16">
+          <div className="flex flex-row space-x-2">
+            {Object.keys(socialMediaIcons).map((icon) => {
+              return (
+                <Image
+                  key={icon}
+                  src={socialMediaIcons[icon]}
+                  alt={`${icon} icon`}
+                  width={25}
+                  height={25}
+                  style={{ width: "25px", height: "25px" }}
+                />
+              );
+            })}
+          </div>
+          <p>© 2024 Karlie Guan. All rights reserved.</p>
+        </div>
       </div>
     </footer>
   );
---
+++ utils/socialMedia-icons.ts
@@ -0,0 +1,9 @@
+type SocialMediaIcons = {
+  [key: string]: string;
+};
+
+export const socialMediaIcons: SocialMediaIcons = {
+  linkedin: "/logos/linkedin/linkedin.svg",
+  github: "/logos/github/github.svg",
+  medium: "/logos/github/github.svg",
+};
