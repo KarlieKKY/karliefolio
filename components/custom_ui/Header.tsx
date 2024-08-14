84b48bc4e17a8d5dcaaf59030ce784d40478bd7d
KarlieKKY <yanndeel@gmail.com>
2024-08-14 17:16:47 +0100

    added banner jpg

--- components/custom_ui/Header.tsx
+++ components/custom_ui/Header.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function HeroSection() {
   return (
-    <div className="bg-black h-96">
-      <h1>hero section</h1>
+    <div className="bg-black text-white">
+      <Image
+        src="/images/banner.JPG"
+        alt="header"
+        className="block w-full object-cover"
+        width={80}
+        height={384}
+      />
     </div>
   );
 }
