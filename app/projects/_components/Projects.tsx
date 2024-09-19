16c0b640b813e4d3132ef5063ed8f78338ccafc7
KarlieKKY <yanndeel@gmail.com>
2024-09-19 17:45:30 +0100

    added project section animation

--- app/projects/_components/Projects.tsx
+++ app/projects/_components/Projects.tsx
@@ -12,9 +12,29 @@ import AllProjects from "./AllProjects";
 import { CornerDownRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
+import { motion, Variants } from "framer-motion";
+
 interface ProjectsProps {
   showAll?: boolean;
 }
+
+const projectVariants: Variants = {
+  offscreen: {
+    opacity: 0,
+    y: 100,
+  },
+  onscreen: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      bounce: 0.6,
+      duration: 0.8,
+      ease: "easeIn",
+      delay: 0.13 * index,
+    },
+  }),
+};
 
 export default function Projects({ showAll = false }: ProjectsProps) {
   const router = useRouter();
@@ -28,79 +48,103 @@ export default function Projects({ showAll = false }: ProjectsProps) {
     <section className="py-8 bg-orange-50 3xl:py-16">
       <div className="px-4 sm:px-6 lg:px-12 3xl:px-48">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-3xl font-bold text-gray-900 uppercase">
-            projects
-          </h2>
-          <Link
-            href="/projects"
-            className="text-red-600 hover:text-red-800 flex flex-row space-x-1.5 items-center"
+          <motion.div
+            initial={{ opacity: 0, y: -30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.3, ease: "easeOut" }}
+            viewport={{ once: true }}
           >
-            <div className="uppercase">GO TO PROJECTS PAGE</div>
-            <CornerDownRight className="w-4 h-4" />
-          </Link>
+            <h2 className="text-3xl font-bold text-gray-900 uppercase">
+              projects
+            </h2>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5, ease: "easeOut" }}
+            viewport={{ once: true }}
+          >
+            <Link
+              href="/projects"
+              className="text-red-600 hover:text-red-800 flex flex-row space-x-1.5 items-center"
+            >
+              <div className="uppercase">GO TO PROJECTS PAGE</div>
+              <CornerDownRight className="w-4 h-4" />
+            </Link>
+          </motion.div>
         </div>
-        <div className="hidden md:grid gap-7 md:grid-cols-3">
-          {projectsToShow.map((project) => (
-            <div key={project.id} className="overflow-hidden">
-              <div className="w-full h-48 relative">
-                <Image
-                  src={project.imageUrl}
-                  alt={project.name}
-                  className="object-cover"
-                  layout="fill"
-                />
-              </div>
-              <div className="text-xs text-gray-500 mb-2">
-                <span className="uppercase">{project.category}</span>{" "}
-                &nbsp;|&nbsp; {convertDate(project.date)}
-              </div>
-              <div className="mt-4">
-                <h3 className="text-lg font-semibold mb-2">{project.name}</h3>
-                <p className="text-gray-700 mb-4">{project.description}</p>
-                <div className="flex flex-row space-x-2 mb-4">
-                  {project.tech.map((tech: string, idx) => (
-                    <div
-                      key={idx}
-                      // className="text-sm text-gray-600 bg-gray-200 px-2 py-1 rounded"
-                    >
-                      <Image
-                        src={getTechIconPath(tech)}
-                        alt={`${tech} Icon`}
-                        width={23}
-                        height={23}
-                        style={{ width: "auto", height: "23px" }}
-                      />
+        <motion.div
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true }}
+        >
+          <div className="hidden md:grid gap-7 md:grid-cols-3">
+            {projectsToShow.map((project, index) => (
+              <motion.div variants={projectVariants} custom={index}>
+                <div key={project.id} className="overflow-hidden">
+                  <div className="w-full h-48 relative">
+                    <Image
+                      src={project.imageUrl}
+                      alt={project.name}
+                      className="object-cover"
+                      layout="fill"
+                    />
+                  </div>
+                  <div className="text-xs text-gray-500 mb-2">
+                    <span className="uppercase">{project.category}</span>{" "}
+                    &nbsp;|&nbsp; {convertDate(project.date)}
+                  </div>
+                  <div className="mt-4">
+                    <h3 className="text-lg font-semibold mb-2">
+                      {project.name}
+                    </h3>
+                    <p className="text-gray-700 mb-4">{project.description}</p>
+                    <div className="flex flex-row space-x-2 mb-4">
+                      {project.tech.map((tech: string, idx) => (
+                        <div
+                          key={idx}
+                          // className="text-sm text-gray-600 bg-gray-200 px-2 py-1 rounded"
+                        >
+                          <Image
+                            src={getTechIconPath(tech)}
+                            alt={`${tech} Icon`}
+                            width={23}
+                            height={23}
+                            style={{ width: "auto", height: "23px" }}
+                          />
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <a
-                    href={project.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="outline" className="bg-transparent">
-                      Github
-                    </Button>
-                  </a>
-                  {/* <a
+                    <div className="flex flex-row space-x-2">
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Button variant="outline" className="bg-transparent">
+                          Github
+                        </Button>
+                      </a>
+                      {/* <a
                     href={project.demo}
                     target="_blank"
                     rel="noopener noreferrer"
                   > */}
-                  <Button
-                    variant="outline"
-                    className="bg-transparent"
-                    onClick={() => handleViewPageClick(project.id)}
-                  >
-                    View Page
-                  </Button>
-                  {/* </a> */}
+                      <Button
+                        variant="outline"
+                        className="bg-transparent"
+                        onClick={() => handleViewPageClick(project.id)}
+                      >
+                        View Page
+                      </Button>
+                      {/* </a> */}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
+              </motion.div>
+            ))}
+          </div>
+        </motion.div>
         <div className="md:hidden">
           {showAll ? (
             <AllProjects />
